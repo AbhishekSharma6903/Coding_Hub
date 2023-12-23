89faf8419b382d725016fa2fd901bdfcd1a8d941
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2023-12-23 14:10:00 +0530

    Created que2.c

---
+++ C_programming/Chapter_2/que2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int n, sum=0;
+    printf("Enter a number : ");
+    scanf("%d", &n);
+    while(n!=0){
+        int rem = n%10;
+        sum = sum*10+rem;
+        n=n/10;
+    }
+    printf("reverse of digit is %d", sum);
+}
