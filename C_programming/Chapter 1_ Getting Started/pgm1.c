a41b4a29e5090a8501d0327dd22de13f201c6919
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2023-12-23 10:50:16 +0530

    started chapter_1

---
+++ C_programming/Chapter 1_ Getting Started/pgm1.c
@@ -0,0 +1,13 @@
+//manual input
+#include<stdio.h>
+
+int main(){
+    int p=1000;
+    int n = 3;
+    float si;
+    float r = 8.5;
+
+    si = (p*n*r)/100;
+    printf("%f\n",si);
+    return 0;
+}
