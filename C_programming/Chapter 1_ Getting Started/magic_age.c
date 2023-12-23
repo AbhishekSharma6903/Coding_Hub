50186505d418c71b5cc4879c16a7a5a02b08eeb7
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2023-12-23 11:07:09 +0530

    Created 3_magic_age.c

---
+++ C_programming/Chapter 1_ Getting Started/magic_age.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+int age;
+  printf("Think of your age : ");
+  scanf("%d",&age);
+  printf("Let me tell your age ... You are %d old" , age);
+return 0;
+}
+
+/*OUTPUT:
+Think of your age : 19
+Let me tell your age ... You are  19 old
+*/
